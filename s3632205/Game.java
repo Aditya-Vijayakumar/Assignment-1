521c087339479a875d046a01c69c7a1e78e375af
Aditya-Vijayakumar <s3632205@student.rmit.edu.au>
2017-04-09 11:29:15 +1000

    Update Game.java

--- s3632205/Game.java
+++ s3632205/Game.java
@@ -1,42 +1,39 @@
-//Written by Aditya Vijayakumar
-//Student ID: s3632205
-
-package s3632205;
-
-import java.util.Random;
-
-public class Game
+ class Game 
 {
+		
+	//Written by Aditya Vijayakumar
+	//Student ID: s3632205
+
 
 	public String gameId;
 	public Official referee;
 	public Athlete[] participants=new Athlete[8];
 	public String userPrediction=null;
 	public Athlete[] winners=null;
-
+	
 	public Game chooseAthletes(Game game,String choose){
-
+		
 		this.participants=AthleteDetails.chooseAthletes(choose);
 		return game;
 	}
 	public Game createGameId(Game game,String option)
 	{
-		Random r=new Random();
-		int randomId=r.nextInt((99-10)+1)+10;
-		if(option.equals("1"))
-		{
+		Random r= new Random();
+	    int randomId= r.nextInt((99-10)+1)+10;
+	    if(option.equals("1")){
 			game.gameId="S"+randomId;
+			
 		}
 		else if(option.equals("2"))
 		{
 			game.gameId="R"+randomId;
+			
 		}
-		else if(option.equals("3"))
-		{
+		else {
 			game.gameId="C"+randomId;
+			
 		}
-		return game;
+	    return game;
 	}
-
-}
+	}
 
